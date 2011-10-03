202b4a8b25ddb4040df7086c636fa52fcf6cfd60
Yevgeniy A. Viktorov <yeevgen@gmail.com>
2011-10-03 21:54:37 +0300

    cleanup default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,19 +1,12 @@
 # General settings
 default[:magento][:version] = "stable"
-default[:magento][:downloader][:url] = "http://www.magentocommerce.com/downloads/assets/1.3.2.1/magento-downloader-1.3.2.1.tar.gz"
-default[:magento][:downloader][:checksum] = "91ccdebf0403f0c328cb728b4cd19504"
-set[:magento][:dir] = "/var/www/magento"
+default[:magento][:dir] = "/var/www/magento"
+default[:magento][:user] = "magento"
 default[:magento][:db][:host] = "localhost"
 default[:magento][:db][:database] = "magentodb"
 default[:magento][:db][:username] = "magentouser"
 default[:magento][:admin][:email] = "webmaster@localhost"
 default[:magento][:admin][:user] = "admin"
-
-default[:magento][:sample_data][:url] = "http://www.magentocommerce.com/downloads/assets/1.2.0/magento-sample-data-1.2.0.tar.gz"
-default[:magento][:sample_data][:checksum] = "bb53b15c081e1f437ffb9a31178b9db8"
-
-default[:magento][:server][:aliases] = Array.new
-default[:magento][:server][:static_domains] = Array.new
 
 ::Chef::Node.send(:include, Opscode::OpenSSL::Password)
 
