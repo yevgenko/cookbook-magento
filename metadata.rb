0e1f0e9a0f5e2c1154f424179b1ae1c944538a92
Yevgeniy A. Viktorov <craftsman@yevgenko.me>
2012-07-26 15:45:39 +0300

    update contacts

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "Yevgeniy Viktorov"
-maintainer_email "yeevgen@gmail.com"
+maintainer_email "craftsman@yevgenko.me"
 license          "Apache 2.0"
 description      "Magento app stack"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
