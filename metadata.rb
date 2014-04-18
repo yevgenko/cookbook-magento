a0a98e6e656041387a6562ed18a0ab85464979af
Yevgeniy A. Viktorov <craftsman@yevgenko.me>
2014-04-18 03:43:34 +0300

    Version bump to 0.7.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'craftsman@yevgenko.me'
 license 'Apache 2.0'
 description 'Magento app stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.6.3'
+version '0.7.0'
 recipe 'magento', 'Prepares app stack for magento deployments'
 
 %w(debian ubuntu centos redhat fedora amazon).each do |os|
