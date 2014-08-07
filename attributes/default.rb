4ba431deadd99cbb3c12fe91777f1eebf4aa6360
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-07 17:49:33 -0500

    Setting default install version to 1.9

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,15 @@
 # coding: utf-8
 # General settings
 default[:magento][:url] = 'http://www.magentocommerce.com/downloads/assets/1.'\
-                          '8.1.0/magento-1.8.1.0.tar.gz'
+                          '9.0.1/magento-1.9.0.1.tar.gz'
 default[:magento][:dir] = '/var/www/magento'
 default[:magento][:domain] = node['fqdn']
 # Magento CE's sample data can be found here:
-# 'http://www.magentocommerce.com/downloads/assets/1.6.1.0/magento-sample-dat'\
-# 'a-1.6.1.0.tar.gz'
+# 'http://www.magentocommerce.com/downloads/assets/1.9.0.0/magento-sample-dat'\
+# 'a-1.9.0.0.tar.gz'
+# If you are using a version of Magento Community Edition older than 1.9.0.0,
+# you will need to use a version of sample data that is compatible with your
+# version.
 default[:magento][:sample_data_url] = ''
 default[:magento][:run_type] = 'store'
 default[:magento][:run_codes] = []
