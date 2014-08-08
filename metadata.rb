3ac48ca697fa506a45c4eb0e5f99036537798da0
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-08 12:50:50 -0500

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'craftsman@yevgenko.me'
 license 'Apache 2.0'
 description 'Magento app stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.8.5'
+version '0.8.6'
 recipe 'magento', 'Prepares app stack for magento deployments'
 
 %w(debian ubuntu centos redhat fedora amazon).each do |os|
